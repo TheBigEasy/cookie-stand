cb4d588301fd17e4becb1d61fb2f027f28c0f28d
John Sanford <johnsanfordstudio@gmail.com>
2015-10-07 20:21:45 -0700

    Changed from 1 array and a score keeping variable to 2 arrays and a third function to calculate totals.

--- app.js
+++ app.js
@@ -1,58 +1,54 @@
 
 
 var CookieStand = function(place,minCustHour,maxCustHour,avgCookiesCust,id) {
-	this.place = place;
-   // location: 'Pike Place Market',
-  // minCustHour: 17,
+	
+  this.place = place; 
   this.minCustHour = minCustHour;
-  // maxCustHour: 88,
   this.maxCustHour = maxCustHour;
-  // avgCookiesCust: 5.2,
   this.avgCookiesCust = avgCookiesCust;
   this.id = id;
+  this.cookiesPerHour = [];
+  this.openHours = ["10am","11am","12pm","1pm","2pm","3pm", "4pm","5pm"];
   
 
-
-
-  // cookiesPerDay: 0,
-  this.cookiesPerDay = 0;
 
   this.randCustHour = function() {
     return Math.random() * (this.maxCustHour - this.minCustHour + 1) + this.minCustHour;
     };
 
   this.avgCookiesHour = function() {
-  var cookies = Math.floor(this.randCustHour() * this.avgCookiesCust);
-  this.cookiesPerDay += cookies;
-  	return cookies;
+  return Math.floor(this.randCustHour() * this.avgCookiesCust);
   };
 
-  var stuff1 = ['10am ' + this.avgCookiesHour(),'11am ' + this.avgCookiesHour(),'12pm ' + this.avgCookiesHour(),'1pm ' + this.avgCookiesHour(),'2pm ' + this.avgCookiesHour(),'3pm ' + this.avgCookiesHour(),'4pm ' + this.avgCookiesHour(),'5pm ' + this.avgCookiesHour(), 'total ' + this.cookiesPerDay];
+  this.totalDailyCookies = function() {
+    this.dailyTotals = 0;
+    for (var i = 0; i < this.openHours.length; i++) {
+      this.cookiesPerHour.push(this.avgCookiesHour());
+      this.dailyTotals = this.dailyTotals + this.cookiesPerHour[i];
+    };
 
- this.makeUL = function(array) {
+    return this.dailyTotals;
+  };
 
-    for(var i = 0; i < array.length; i++) {
+ this.makeUL = function(id) {
+
+    for(var i = 0; i < this.openHours.length; i++) {
 
         // connect the list to the JS; this is where your list will appear
-        var awesomeList = document.getElementById(this.id);
+        var storeList = document.getElementById(this.id);
         
         // Create the list item:
         var item = document.createElement('li');
 
         // Set its contents:
-        item.appendChild(document.createTextNode(array[i]));
+        item.appendChild(document.createTextNode(this.openHours[i] + ': ' + this.avgCookiesHour() ));
 
         // Add it to the list:
-        awesomeList.appendChild(item);
-
-
-    }
-
-    // Finally, return the constructed list:
-    return awesomeList;
+        storeList.appendChild(item);
+        }
 
   };
-  this.makeUL(stuff1);	
+        this.makeUL(this.id);	
  
 } 
 
@@ -63,32 +59,4 @@ var bellevueSquare = new CookieStand('bellevueSquare', 20, 48, 3.3, 'bSquare');
 var alkiBeach = new CookieStand('alkiBeach', 3, 24, 2.6, 'alki');
 
 
-
-// function makeUL(array) {
-
-//     for(var i = 0; i < array.length; i++) {
-
-//         // connect the list to the JS; this is where your list will appear
-//         var awesomeList = document.getElementById('id');
-        
-//         // Create the list item:
-//         var item = document.createElement('li');
-
-//         // Set its contents:
-//         item.appendChild(document.createTextNode(array[i]));
-
-//         // Add it to the list:
-//         awesomeList.appendChild(item);
-
-
-//     }
-
-//     // Finally, return the constructed list:
-//     return awesomeList;
-// }
-
-// var stuff1 = ['10am ' + pikePlace.avgCookiesHour(),'11am ' + pikePlace.avgCookiesHour(),'12pm ' + pikePlace.avgCookiesHour(),'1pm ' + pikePlace.avgCookiesHour(),'2pm ' + pikePlace.avgCookiesHour(),'3pm ' + pikePlace.avgCookiesHour(),'4pm ' + pikePlace.avgCookiesHour(),'5pm ' + pikePlace.avgCookiesHour(), 'total ' + pikePlace.cookiesPerDay];
-
-
-// makeUL(stuff1);
 
